94c2a4a984d1f9fc0e5f8023383d994edef54bba
Alexander Rodriguez <rodriguez.alexander223@gmail.com>
2023-10-10 18:23:56 -0400

    multiply two numbers

---
+++ js101-js119/easy2/3.multiplying_two_numbers.js
@@ -0,0 +1,34 @@
+/*
+Create a function that takes two arguments, multiplies them together, and 
+returns the result.
+*/
+
+/*
+Input:
+- two numbers
+
+Output:
+- the result from multiplying the two numbers
+
+Rules:
+
+Mental Model:
+
+Examples/test Cases:
+console.log(multiply(5, 3) === 15); // logs true
+
+Data Structures:
+- none
+
+Algorithm:
+- write a function that takes two numbers as arguments
+- multiply the two numbers
+- return the result
+*/
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+console.log(multiply(2, 2)); // 4
+console.log(multiply(5, 2) === 10); // true
