c5b8aba1da1090c6d4bab63032bee8ba46e80610
Alexander Rodriguez <rodriguez.alexander223@gmail.com>
2023-11-27 23:01:44 -0500

    cleanup the code and fix monthly payment amount

--- js101-js119/lesson2/loan_calculator.js
+++ js101-js119/lesson2/loan_calculator.js
@@ -1,18 +1,28 @@
-let loanAmount = 50000;
-let annualPercentageRate = 9.06;
-let loanDuration = 72; // months
+let readlineSync = require("readline-sync");
 
-let monthlyInterestRate = annualPercentageRate / 12;
+console.log("Enter the loan amount: ");
+let loanAmount = Number(readlineSync.question());
+
+console.log("Enter the interest rate: ");
+let interestRate = Number(readlineSync.question());
+
+console.log("Enter the loan duration (Years) ");
+let years = Number(readlineSync.question());
+
+let annualInterestRate = interestRate / 100;
+let monthlyInterestRate = annualInterestRate / 12;
+let months = years * 12;
 
 let monthlyAmount = loanAmount * (
   monthlyInterestRate / (
-    1 - Math.pow((1 + monthlyInterestRate), (-loanDuration))
+    1 - Math.pow((1 + monthlyInterestRate), (-months))
   )
 );
+
 
 let monthlyPayment = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD",
 }).format(monthlyAmount);
 
-console.log(monthlyPayment);
+console.log(`Monthly Payment: ${monthlyPayment}`);
