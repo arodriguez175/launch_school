045e22a5592e13be13dbe93f4a85f3d1d8f8dc3b
Alexander Rodriguez <rodriguez.alexander223@gmail.com>
2023-11-29 15:40:46 -0500

    add input validation and make monthly payment a number to prevent future problems

--- js101-js119/lesson2/loan_calculator.js
+++ js101-js119/lesson2/loan_calculator.js
@@ -1,28 +1,44 @@
 let readlineSync = require("readline-sync");
 
-console.log("Enter the loan amount: ");
-let loanAmount = Number(readlineSync.question());
+function isInvalid(number) {
+  return number.trim() === '' || isNaN(number) || number < 0;
+}
 
-console.log("Enter the interest rate: ");
-let interestRate = Number(readlineSync.question());
+console.log("Enter the loan amount (Example: 50000 for $50,000): ");
+let loanAmount = readlineSync.question();
 
-console.log("Enter the loan duration (Years) ");
-let years = Number(readlineSync.question());
+while (isInvalid(loanAmount)) {
+  console.log('Must enter a positive number');
+  console.log("Enter the loan amount (Example: 50000 for $50,000): ");
+  loanAmount = readlineSync.question();
+}
 
-let annualInterestRate = interestRate / 100;
+console.log("Enter the interest rate (Example: 5 for 5%): ");
+let interestRate = readlineSync.question();
+
+while (isInvalid(interestRate)) {
+  console.log('Must enter a positive number');
+  console.log("Enter the interest rate (Example: 5 for 5%): ");
+  interestRate = readlineSync.question();
+}
+
+console.log("Enter the loan duration (Years): ");
+let years = readlineSync.question();
+
+while (isInvalid(years)) {
+  console.log('Must enter a positive number');
+  console.log("Enter the loan duration (Years): ");
+  years = readlineSync.question();
+}
+
+let annualInterestRate = Number(interestRate) / 100;
 let monthlyInterestRate = annualInterestRate / 12;
-let months = years * 12;
+let months = Number(years) * 12;
 
-let monthlyAmount = loanAmount * (
+let monthlyPayment = Number(loanAmount) * (
   monthlyInterestRate / (
     1 - Math.pow((1 + monthlyInterestRate), (-months))
   )
 );
 
-
-let monthlyPayment = new Intl.NumberFormat("en-US", {
-  style: "currency",
-  currency: "USD",
-}).format(monthlyAmount);
-
-console.log(`Monthly Payment: ${monthlyPayment}`);
+console.log(`Monthly Payment: $${String(monthlyPayment.toFixed(2))}`);
